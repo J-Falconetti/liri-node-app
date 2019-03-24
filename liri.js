56ff33c4bc383cd442a5d1c074c343a54a35cf12
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-24 00:38:35 -0400

    stated switch commands

--- liri.js
+++ liri.js
@@ -1,12 +1,32 @@
+
 require("dotenv").config();
-var keys = require("./keys.js")
+var keys = require('./keys');
+var moment = require('moment');
+var fs = require('fs');
+var axios = require('axios')
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+if (!process.argv[3] === undefined) {
+    var input = process.argv.slice(3).join(' ');
+    console.log(command)
+}
+runLiRi(command, input);
+function runLiRi(command, input) {
+    switch (command) {
+        case 'concert-this':
+                runBandsInTown(input);
+            break;
+        case 'spotify-this-song':
+            input === undefined ? runSpotify("The Sign") :
+                runSpotify(input);
+            break;
+        case 'movie-this':
+            input === undefined ? runOMDB('Mr. Nobody') :
+                runOMDB(input);
+            break;
+        case 'do-what-it-says':
+            
+};
+};
 
-// getting needed nodes and adding key from key.js file
-var spotifyapi = require ('node-spotify-api');
-var request = require ('request');
-var moment = require ('moment');
-var fs = require ('fs')
-var spotify = new spotifyapi(keys.spotify);
-
-var user = process.argv
-console.log(user)
