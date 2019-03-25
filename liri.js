4c9d485d946b444fd93090c7fb9558c4fd290f0b
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-24 21:53:57 -0400

    movies working need to add rest of questions moving on Bands

--- liri.js
+++ liri.js
@@ -7,8 +7,8 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 // if (!process.argv[3] === undefined) {
-var input = process.argv.slice(3).join(' ');
-console.log(input)
+var input = process.argv.slice(3).join('+');
+// console.log(input)
 // }
 runLiRi(command, input);
 
@@ -29,6 +29,7 @@ function runLiRi(command, input) {
 
     };
 };
+// start Spotify
 // this is for the spotify function using node-spoify-api
 function runSpotify(query) {
     // console.log(query)
@@ -39,38 +40,58 @@ function runSpotify(query) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        // loging resuts
+        console.log("Artist: ", data.tracks.items[0].album.artists[0].name);
+        console.log("Song Name: ", data.tracks.items[0].name);
+        console.log("link to song: ", data.tracks.items[0].preview_url)
+        console.log("This song is from: ", data.tracks.items[0].album.name)
 
-
-          
-       
-        console.log("Artist: ",data.tracks.items[0].album.artists[0].name );
-        console.log("Song Name: ",data.tracks.items[0].name);
-        console.log("link to song: ",data.tracks.items[0].preview_url )
-        console.log("This song is from: ",data.tracks.items[0].album.name)
-        
-       
-   
-       
-   
-        // * The album that the song is from
-   
+        // end spotify
     });
-
-
+}
+// start OMDB---NOW WITH AXIOS!
+function runOMDB(query) {
+    // console.log(query)
 }
 
-// axios
-//   .get("http://www.omdbapi.com/?apikey=[yourkey]&")
-//   .then(function(response) {
-//     // If the axios was successful...
-//     // Then log the body from the site!
-//     console.log(response.data);
-//   })
-//   .catch(function(error) {
-//     if (error.response) {
-//       // The request was made and the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log(error.response.data);
-//       console.log(error.response.status);
-//       console.log(error.response.headers);
-// }
+axios.get("http://www.omdbapi.com/?t=" + input + "&apikey=trilogy&")
+    .then(function (response) {
+
+        var movie=response.data
+
+        console.log(movie.data);
+        console.log("===================================================")
+        console.log(movie.Ratings[1])
+        console.log("============================================")
+        console.log(input);
+
+        // response
+        console.log("Movie Title: ",movie.Title);
+        console.log("Reliease Year",movie.Year);
+        console.log("IMBD Rating:",movie.imdbRating);
+        console.log("Rotten Tomatoes Rating: ",movie.Ratings[1])
+    })
+    .catch(function (error) {
+        console.log(error);
+
+        console.log("________________________________________testing area_______________________")
+        console.log(response);
+        console.log(ouput);
+        console.log("________________________________________testing area_______________________")
+    });
+
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+
+// ombd END
+// Bands in town
+function runOMDB(query) {
+    // console.log(query)
+}
+
+axios.get("http://www.omdbapi.com/?t=" + input + "&apikey=trilogy&")
+    .then(function (response) {
+
